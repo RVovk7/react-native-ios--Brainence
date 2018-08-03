4742c1d871394499afc2074dbbda0ae7b74b8625
RVovk7 <RVovk7@gmail.com>
2018-08-03 19:41:16 +0300

    bug fix

--- components/LoginPage.js
+++ components/LoginPage.js
@@ -1,52 +1,70 @@
-import React,{ Component } from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { StyleSheet, Text, View, TextInput, Button, AlertIOS } from 'react-native';
 
 export default class LoginPage extends Component {
     static propTypes = {
         auth: PropTypes.func.isRequired,
-      };
+    };
 
     constructor(props) {
-    
+
         super(props)
         this.state = {
-          logID: '',
+            logID: '',
+            logFail: false,
+            timeClear: ''
         }
-      }
+    }
 
     logClick = () => {
-        const { props: { auth }, state: {logID }} = this;
-        auth(logID);
-      }
-      loginInput = e => {
-          if (!isNaN(+e)){
-              this.setState({
-                  logID: e,
-              })
-          }
-      }
-    render(){
-        const { state : {logID}, logClick, loginInput} = this;
+        const { props: { auth }, state: { logID } } = this;
+       
+        /[0-9]/g.test(logID) && +logID <= 10 ? auth(logID) : this.setState({ logFail: true });
+       
+        const timeClear = setTimeout(() => {
+            this.setState({ logFail: false, logID: '' })
+        }, 1000);
+        this.setState({ timeClear })
+
+
+    }
+
+    loginInput = e => {
+        if (!isNaN(+e)) {
+            this.setState({
+                logID: e,
+            })
+        }
+    }
+
+    componentWillUnmount() {
+        const { timeClear } = this.state;
+        clearTimeout(timeClear);
+    }
+    render() {
+        const { state: { logID, logFail }, logClick, loginInput } = this;
         return (
             <View style={styles.logMain}>
-            <Text style={styles.logLabel}> Enter your ID </Text>
-            <TextInput style={styles.logInput}
-              style={styles.logInput}
-              onChangeText={loginInput}
-              value={logID}
-            />
-            <Button
-                onPress={logClick}
-                title="Sign In"
-                color="black"
-                accessibilityLabel="loginButton"
-        />
-          </View>
-        
-          )
+                <Text style={[{ color: 'red' }, { opacity: logFail ? 100 : 0 }]}> Only numeric from 1 to 10 </Text>
+
+                <Text style={styles.logLabel}> Enter your ID </Text>
+                <TextInput style={styles.logInput}
+                    style={styles.logInput}
+                    onChangeText={loginInput}
+                    value={logID}
+                />
+                <Button
+                    onPress={logClick}
+                    title="Sign In"
+                    color="black"
+                    accessibilityLabel="loginButton"
+                />
+            </View>
+
+        )
     }
- 
+
 }
 LoginPage.propTypes = {
     controledInput: PropTypes.func,
@@ -55,22 +73,22 @@ LoginPage.propTypes = {
 }
 
 const styles = StyleSheet.create({
-       logMain: {
-         flex: 1,
-         backgroundColor: '#fff',
-         alignItems: 'center',
-         justifyContent: 'center',
-       },
-       logLabel: {
+    logMain: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logLabel: {
         fontSize: 25,
         marginBottom: 10,
-       },
-       logInput: {
-         height: 40,
-          width: 150,
-          borderRadius: 5,
-           borderColor: 'gray',
-            borderWidth: 1, 
-            marginBottom: 10,
-       }
-     });
+    },
+    logInput: {
+        height: 40,
+        width: 150,
+        borderRadius: 5,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+    }
+});
--- components/PhotosLists.js
+++ components/PhotosLists.js
@@ -1,41 +1,70 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import PropTypes from 'prop-types'
-import { StyleSheet, Text, View, Image, ScrollView, TouchableHighlight } from 'react-native';
+import { AppRegistry, Text, View, Image, ScrollView, TouchableHighlight, ImagePickerIOS, AsyncStorage } from 'react-native';
 import Head from './Head';
-export default function Photo(props) {
-    const { photosList, styles, logoutClick, albumTitle, navigate } = props.navigation.state.params;
-    return (
-        <Fragment>
-            <Head albumTitle={albumTitle} logoutClick={logoutClick} />
-            <ScrollView >
-                <View style={styles.albumsContainer} >
-                    <TouchableHighlight onPress={() => console.log('click')}>
-                        <Image
-                            style={{ width: 180, height: 180 }}
-                            source={{ uri: 'https://cdn4.iconfinder.com/data/icons/ios7-essence/22/add_plus-512.png' }}
-                        />
-                    </TouchableHighlight>
-                    {photosList.map(a => <View key={a.id} style={styles.album}>
-                        <TouchableHighlight onPress={() => navigate('PhotoScreen', {
-                            photo: a.url,
-                            photoTitle: a.title,
-                            logoutClick: logoutClick
-                        })}>
+export default class Photo extends Component {
+    constructor(props){
+        super(props)
+        this.state= {
+            imageAdd:false,
+        }
+    }
+    imagePicker = () => {
+        const { photosList } = this.props.navigation.state.params;
+    ImagePickerIOS.openSelectDialog(
+            {
+                showImages: true
+            },
+            (imageUri) => {
+                photosList.push({
+                    albumId: photosList.albumId,
+                    id: Date.now(),
+                    title: "new photo",
+                    url: imageUri,
+                    thumbnailUrl: imageUri,
+                  })
+                  this.setState({imageAdd: !this.state.imageAdd})
+            },
+            m => console.log(m)
+          );
+          
+    }
+    render(){
+        const { photosList, styles, logoutClick, albumTitle, navigate } = this.props.navigation.state.params;
+        return (
+            <Fragment>
+                <Head albumTitle={albumTitle} logoutClick={logoutClick} />
+                <ScrollView >
+                    <View style={styles.albumsContainer} >
+                        <TouchableHighlight onPress={this.imagePicker}>
                             <Image
                                 style={{ width: 180, height: 180 }}
-                                source={{ uri: a.thumbnailUrl }}
+                                source={{ uri: 'https://cdn4.iconfinder.com/data/icons/ios7-essence/22/add_plus-512.png' }}
                             />
                         </TouchableHighlight>
-                        <Text style={styles.albumName} >{a.title}</Text>
-                    </View>)
-                    }
-                </View>
-            </ScrollView>
-        </Fragment>
-    )
+                        {photosList.map(a => <View key={a.id} style={styles.album}>
+                            <TouchableHighlight onPress={() => navigate('PhotoScreen', {
+                                photo: a.url,
+                                photoTitle: a.title,
+                                logoutClick: logoutClick
+                            })}>
+                                <Image
+                                    style={{ width: 180, height: 180 }}
+                                    source={{ uri: a.thumbnailUrl }}
+                                />
+                            </TouchableHighlight>
+                            <Text style={styles.albumName} >{a.title}</Text>
+                        </View>)
+                        }
+                    </View>
+                </ScrollView>
+            </Fragment>
+        ) 
+    }
+    
 }
 Photo.propTypes = {
     photosList: PropTypes.array,
 }
-
+AppRegistry.registerComponent('CameraRollPicker', () => CameraRollPicker)
 
